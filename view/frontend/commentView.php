068876811b5fa9cb175406fc5f5c7275dbaa1794
Sarah Garrigue <s_garrigue@hotmail.fr>
2020-11-29 23:30:15 +0100

    TP PHP/MySQL

--- view/frontend/commentView.php
+++ view/frontend/commentView.php
@@ -34,10 +34,11 @@ $comments->closeCursor();
 
 <form class="commentForm" action="index.php?action=addComment" method="post">
 <p>
-    <input type="text" placeholder="Pseudo" value="<?php if (isset($_SESSION['id']) AND isset($_SESSION['pseudo'])) { echo $_SESSION['pseudo']; } ?>" required><br />
+    <label class="title" type="title">laisser un commentaire</label><br />
+    <input type="text" placeholder="Pseudo" value="<?php if (isset($_SESSION['id']) AND isset($_SESSION['pseudo'])) { echo $_SESSION['pseudo']; } ?>" required>
     <input type="hidden" id="idPost" name="idPost" value="<?php if (isset($_GET['id'])) echo $_GET['id']; ?>">
     <input type="hidden" id="idMember" name="idMember" value="<?php if (isset($_SESSION['id'])) { echo $_SESSION['id']; } ?>">
-    <textarea class="postComment" id="comment" name="comment" rows="7" cols="30" placeholder="votre message" required></textarea>
+    <textarea class="postComment" id="comment" name="comment" rows="7" cols="30" placeholder="votre message" required></textarea><br />
     <input type="submit" name="submit" value="Valider">
 </p>
 </form>
